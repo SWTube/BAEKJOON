12a2c12bd06ceb9fab66c5956b419a6b26f12140
bluehyena <42088479+bluehyena@users.noreply.github.com>
2021-02-23 01:02:52 +0900

    [Add] - Add 2751

---
+++ 2751/LeeJunHyeok_2751.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1000001];
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; ++i) {
+		cout << arr[i] << '\n';
+	}
+	return 0;
+}
